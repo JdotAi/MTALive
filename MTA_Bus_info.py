bb3b47244146007105618f1ae611604dc0e3789b
Jay Patel <jaypaatel42@gmail.com>
2025-01-31 18:22:20 -0500

    This is how to view the data

--- MTA_Bus_info.py
+++ MTA_Bus_info.py
@@ -1,1 +1,12 @@
 from config import MTA_API_KEY
+import requests
+bus_route = 'M5'  # Example bus route
+
+# Endpoint for the MTA Bus Time API
+url = f'http://bustime.mta.info/api/siri/vehicle-monitoring.json?key={MTA_API_KEY}&VehicleMonitoringDetailLevel=calls&LineRef={bus_route}'
+
+response = requests.get(url)
+data = response.json()
+
+# Assuming the API and key are correct, this will print the data retrieved
+print(data)
